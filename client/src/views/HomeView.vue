403e4b3c629cb7a41e8cc6b518bc80c6afbd6ab2
Alexsander P.F. Santos <82063944+Alexsander33set@users.noreply.github.com>
2023-11-29 06:24:26 -0300

    start homepage template api requisition

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,32 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
+import axios from 'axios'
+
+export default {
+  name:"HomeView",
+  data() {
+    return {
+      response: {}
+    }
+  },
+  created() {
+    
+  },
+  methods: {
+    requestAPI(){
+      axios.get('api/weather-data', { params: { lat: "-22", lon:"-44", lang:"pt_br", } })
+      .then(res=> {this.response = res})
+    }
+  },
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <button @click="requestAPI"> Get API Data</button>
+    <hr>
+    <h3>Response:</h3>
+    <div>
+      {{ response }}
+    </div>
   </main>
 </template>
